a777f9cfc0c9260011465e2eb8e5ff4c145e4c05
Unknown <pikostis@gmail.com>
2017-12-08 19:15:54 +0200

    test Book object

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,13 +1,38 @@
 package com.twu.biblioteca;
 
+import java.util.Date;
+
 public class Book {
     private String bookAuthor;
+    private Date bookReleaseDate;
+    private String bookTitle;
 
-    public Book(String bookAuthor) {
+    public void setBookAuthor (String bookAuthor) {
         this.bookAuthor = bookAuthor;
     }
 
     public String getAuthor() {
         return this.bookAuthor;
+    }
+
+    public void setReleaseDate(Date date) {
+        this.bookReleaseDate = date;
+    }
+
+    public Date getRealeaseDate() {
+        return this.bookReleaseDate;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    @Override
+    public String toString() {
+        return bookTitle + " | " + bookAuthor + " | " + bookReleaseDate;
     }
 }
--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -1,6 +1,9 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
+
+import java.util.Date;
+
 import static junit.framework.TestCase.assertEquals;
 
 
@@ -8,8 +11,36 @@ public class BookTest {
 
     @Test
     public void shouldReturnBookAuthor() {
-        Book book = new Book("John");
+        Book book = new Book();
+        book.setBookAuthor("John");
         assertEquals(book.getAuthor(), "John");
+    }
+
+    @Test
+    public void shouldReturnBookReleaseDate() {
+        Book book = new Book();
+        book.setReleaseDate(new Date(1990,2,23));
+        assertEquals(book.getRealeaseDate(), new Date(1990, 2, 23));
+    }
+
+    @Test
+    public void shouldReturnBookTitle() {
+        Book book = new Book();
+        String bookTitle = "TDD Kent Beck";
+        book.setBookTitle(bookTitle);
+        assertEquals(book.getBookTitle(), bookTitle);
+    }
+
+    @Test
+    public void shouldReturnStringRepresentationOfBook() {
+        Book book = new Book();
+
+        String bookTitle = "TDD by Example";
+        book.setBookTitle(bookTitle);
+        book.setReleaseDate(new Date(1990,2,23));
+        book.setBookAuthor("Kent Beck");
+
+        assertEquals(book.toString(), book.getBookTitle() + " | " + book.getAuthor() + " | " + book.getRealeaseDate());
     }
 
 }
